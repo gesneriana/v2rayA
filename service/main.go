d6ac4aa2b5fac0b6dad18a9d7ca49abba1854313
gesneriana <s694060865@gmail.com>
2022-07-14 09:35:26 +0800

    修复蓝屏bug, v2raya启动了tun虚拟网卡之后不能使用cmd命令强制结束, 启动时回收资源即可，或者使用exec命令重启自身

--- service/main.go
+++ service/main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
 	"runtime"
-	"syscall"
 
 	"github.com/v2rayA/v2rayA/common/netTools"
 
@@ -35,23 +31,7 @@ func main() {
 	checkUpdate()
 	hello()
 
-	go func() {
-		if err := run(); err != nil {
-			log.Fatal("main: %v", err)
-		}
-	}()
-
-	if runtime.GOOS == "windows" {
-		// 监控两个信号
-		// TERM信号（kill + 进程号 触发）
-		// 中断信号（ctrl + c 触发）
-		osc := make(chan os.Signal, 1)
-		signal.Notify(osc, syscall.SIGTERM, syscall.SIGINT)
-		s := <-osc
-		fmt.Println("监听到退出信号,s=", s)
-
-		// 退出前的清理操作
-		netTools.CloseTun()
-		log.Info("v2raya server is stop")
+	if err := run(); err != nil {
+		log.Fatal("main: %v", err)
 	}
 }
