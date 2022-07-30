5c5842e6a227964c732fa035a26f73f42d31831a
gesneriana <s694060865@gmail.com>
2022-07-30 23:54:57 +0800

    优化获取出站ip的代码

--- service/common/netTools/route.go
+++ service/common/netTools/route.go
@@ -2,12 +2,35 @@ package netTools
 
 import (
 	"fmt"
-	"strings"
-
 	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/v2rayA/v2rayA/common/cmds"
+	"github.com/v2rayA/v2rayA/pkg/util/log"
+	"net"
+	"strings"
 )
 
+// GetOutBoundIP 获取默认的出站ip, 多个网卡也能正常获取, 如果这时候恰好网络连接断开了就获取不到了, 如果已经用clash开启了tun代理也获取不到, 获取的ip是198.18.0.1, 这个是错误的
+func GetOutBoundIP() (ip string, err error) {
+	var addrSlice = []string{"223.6.6.6", "119.29.29.29", "8.8.8.8", "1.1.1.1", "208.67.222.222", "208.67.220.220", "8.8.4.4", "162.14.21.56", "162.14.21.178", "175.24.154.66"}
+	for _, addr := range addrSlice {
+		conn, err2 := net.Dial("udp", addr+":53")
+		err = err2
+		if err2 != nil {
+			log.Error("GetOutBoundIP err:", err2)
+			continue
+		}
+		localAddr := conn.LocalAddr().(*net.UDPAddr)
+		log.Info("GetOutBoundIP local ip:", localAddr.String())
+		ip = strings.Split(localAddr.String(), ":")[0]
+		if len(ip) > 0 {
+			return
+		}
+	}
+
+	return
+}
+
+// GetGatewayIp 获取网关ip和出站网卡的ip
 func GetGatewayIp() (gatewayIp string, ip string) {
 	var result = cmds.ExecCmd("chcp 65001 & ipconfig")
 	lines := strings.Split(result, "\n")
@@ -37,6 +60,7 @@ func GetGatewayIp() (gatewayIp string, ip string) {
 			}
 		}
 	}
+
 	if len(gatewayIp) > 0 {
 		getewaySlice := strings.Split(gatewayIp, ".")
 		if len(getewaySlice) != 4 {
@@ -46,11 +70,16 @@ func GetGatewayIp() (gatewayIp string, ip string) {
 		prefixString := fmt.Sprintf("%s.%s.%s", getewaySlice[0], getewaySlice[1], getewaySlice[2])
 		for _, ipString := range ipSlice {
 			if strings.HasPrefix(ipString, prefixString) {
-				ip = ipString // 使用匹配的 网关和网卡ip, 如果与多个网卡, 请考虑手动指定网关ip和网卡ip
+				ip = ipString // 使用匹配的 网关和网卡ip, 如果有多个网卡, 请考虑手动指定网关ip和网卡ip
 				break
 			}
 		}
 	}
+
+	if len(ip) == 0 {
+		ip, _ = GetOutBoundIP()
+	}
+
 	return
 }
 
